19dc6a8b5cc42a384e0c456b5c1afbb083750f66
foxt451 <svpetrenko123@gmail.com>
2022-05-23 20:16:42 +0300

    handle ipv6

--- 05-geoip/controllers/ip.js
+++ 05-geoip/controllers/ip.js
@@ -5,12 +5,16 @@ import {
   intToIp
 } from "../utils/getCountryByIp.js";
 
+import {isIPv6} from "net";
+
 const ipRegistryPath = "IP2LOCATION-LITE-DB1.CSV";
 const registry = await initIpRegistry(ipRegistryPath);
 
 const getUserCountry = (req, res) => {
   const record = getRegistryRecordByIp(ipToInt(req.ip), registry);
-  console.log(req.ip);
+  if (isIPv6(req.ip)) {
+    return res.json({msg: "IPv6 is not supported"});
+  }
   res.json({ ip: req.ip, ...record, start: intToIp(record.start), end: intToIp(record.end) });
 };
 
